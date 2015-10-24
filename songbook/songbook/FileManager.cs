fa8e124f83c7d598574e1108fda05f60fdc33a3e
MaxWellHays <MaxWellHays.ru@gmail.com>
2015-10-24 17:12:37 +0300

    Added FileManager and Song classes

---
+++ songbook/songbook/FileManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace songbook
+{
+    public static class FileManager
+    {
+        private static List<Song> songs;
+        static IEnumerable<Song> Songs
+        {
+            get
+            {
+                if (songs == null)
+                {
+                    InitializeSongs();
+                }
+                return songs;
+            }
+        }
+
+        private static async void InitializeSongs()
+        {
+            byte[] fileBytes = System.Text.Encoding.UTF8.GetBytes("1234567");
+
+            StorageFolder local = ApplicationData.Current.LocalFolder;
+            var dataFolder = await local.CreateFolderAsync("DataFolder", CreationCollisionOption.OpenIfExists);
+
+            var file = await dataFolder.CreateFileAsync("DataFile.txt", CreationCollisionOption.ReplaceExisting);
+
+            using (var s = await file.OpenStreamForWriteAsync())
+            {
+                s.Write(fileBytes, 0, fileBytes.Length);
+            }
+        }
+    }
+}
