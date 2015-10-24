db2a213512d59c830e1a23dd96eafd872423e74d
MaxWellHays <MaxWellHays.ru@gmail.com>
2015-10-24 19:21:34 +0300

    Added FileManager (beta) and Song

--- songbook/songbook/FileManager.cs
+++ songbook/songbook/FileManager.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-using System.Resources;
-using System.Threading.Tasks;
 using Windows.ApplicationModel.Resources;
-using Windows.Storage;
 
 namespace songbook
 {
@@ -24,33 +19,14 @@ namespace songbook
             }
         }
 
-        private static async void InitializeSongs()
+        private static void InitializeSongs()
         {
-            ResourceLoader rl = new ResourceLoader();
-            var t = rl.GetString("TestString");
-            //string s = AppResources.NameOfResource
-            byte[] fileBytes = System.Text.Encoding.UTF8.GetBytes("1234567");
-
-            StorageFolder local = ApplicationData.Current.LocalFolder;
-            var dataFolder = await local.CreateFolderAsync("DataFolder", CreationCollisionOption.OpenIfExists);
-
-            var assembly = typeof(FileManager).GetTypeInfo().Assembly;
-
-            // Use this help aid to figure out what the actual manifest resource name is.
-            string[] resources = assembly.GetManifestResourceNames();
-
-            // Once you figure out the name, pass it in as the argument here.
-            Stream stream = assembly.GetManifestResourceStream("1.XML");
-            while (stream.CanRead)
+            songs = new List<Song>();
+            ResourceLoader resourse = new ResourceLoader();
+            var songNames = resourse.GetString("AllSongs").Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var songName in songNames)
             {
-                var t1 = 1;
-            }
-
-            var file = await dataFolder.CreateFileAsync("DataFile.txt", CreationCollisionOption.ReplaceExisting);
-
-            using (var s = await file.OpenStreamForWriteAsync())
-            {
-                s.Write(fileBytes, 0, fileBytes.Length);
+                songs.Add(new Song(songName));
             }
         }
     }
--- songbook/songbook/Song.cs
+++ songbook/songbook/Song.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace songbook
+﻿namespace songbook
 {
     class Song
     {
+        public readonly string Name;
+
+        private string _text;
+        public string Text
+        {
+            get
+            {
+                if (_text == null)
+                {
+                    _text = null;
+                }
+                return _text;
+            }
+        }
+
+        public Song(string name)
+        {
+            Name = name;
+        }
     }
 }
