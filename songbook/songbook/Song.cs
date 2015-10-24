3548f739569c8a7382890887c848ada9bfb705b8
MaxWellHays <MaxWellHays.ru@gmail.com>
2015-10-24 23:09:22 +0300

    Added Artist and improved Song

--- songbook/songbook/Song.cs
+++ songbook/songbook/Song.cs
@@ -1,8 +1,13 @@
-﻿namespace songbook
+﻿using System;
+using System.Linq;
+
+namespace songbook
 {
     class Song
     {
-        public readonly string Name;
+        public readonly string FullName;
+        public readonly string SongName;
+        public readonly Artist Artist;
 
         private string _text;
         public string Text
@@ -19,7 +24,10 @@
 
         public Song(string name)
         {
-            Name = name;
+            FullName = name;
+            var values = name.Split(new[] {"-"}, StringSplitOptions.RemoveEmptyEntries).Select(s=>s.Trim()).ToList();
+            SongName = values[1];
+            Artist = Artist.GetOrAddArtist(values[0], this);
         }
     }
 }
