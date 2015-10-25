3fd9d5cd4bc9fcfd5a7821daf0be835763dce497
inventive_seven <mr.kakorin@yandex.ru>
2015-10-25 06:56:17 +0300

    vse ok

--- songbook/songbook/SearchBar.cs
+++ songbook/songbook/SearchBar.cs
@@ -15,6 +15,11 @@ using Windows.UI.Xaml.Navigation;
 
 namespace songbook
 {
+    public enum ConditionOframe
+    {
+        None = 0, Dog = 1, Cat = 2, Bird = 4, Rodent = 8,
+        Reptile = 16, Other = 32
+    };
     public class ArtistOrSong
     {
         public string Name;
